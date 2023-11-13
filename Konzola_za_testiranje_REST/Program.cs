108f8a3e09c084d3310982e71fdbf72bc4fac787
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2023-11-13 21:00:46 +0100

    Gotovo s WEB API

---
+++ Konzola_za_testiranje_REST/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+
+namespace Konzola_za_testiranje_REST
+{
+    class Movie
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7223/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/Movies").Result;
+            if(response.IsSuccessStatusCode)
+            {
+                var data=response.Content.ReadAsAsync<IEnumerable<Movie>>().Result;
+                foreach(var m in data)
+                {
+                    Console.WriteLine(m.Title);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            var put_result=client.PostAsync("api/Movies", 
+                    new Movie { Id = "5", Title = "The Godfather", Genre = "Crime", Year = "1972" }, 
+                    new JsonMediaTypeFormatter()).Result;
+            if(put_result.StatusCode==System.Net.HttpStatusCode.Created)
+            {
+                Console.WriteLine("Movie added");
+            }
+            else
+            {
+                Console.WriteLine("Movie not added");
+            }
+            var delete_result=client.DeleteAsync("api/Movies/6").Result;
+            switch (delete_result.StatusCode)
+            {
+                case System.Net.HttpStatusCode.OK:
+                    Console.WriteLine("Movie deleted");
+                    break;
+                case System.Net.HttpStatusCode.NotFound:
+                    Console.WriteLine("Movie not found");
+                    break;
+                case System.Net.HttpStatusCode.InternalServerError:
+                    Console.WriteLine("Internal server error");
+                    break;
+                case System.Net.HttpStatusCode.BadRequest:
+                    Console.WriteLine("Bad request");
+                    break;
+            }
+
+            if ((int)delete_result.StatusCode == 417)
+            {
+
+            }
+
+            client.PutAsync("api/Movies/1", 
+                            new Movie { Id = "1", Title = "The Godfather", Genre = "Crime", Year = "1972" }, 
+                            new JsonMediaTypeFormatter()).Wait();
+        }
+    }
+}
---
+++ Movies.Test/MoviesRepControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.API.Controllers;
+using Movies.Data.Models;
+using Movies.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Test
+{
+    public class MoviesRepControllerTest
+    {
+        private readonly moviesContext _context;
+        private readonly MoviesRepController _controller;
+        private readonly MovieRepository _repository;
+
+        public MoviesRepControllerTest()
+        {
+            _context = new moviesContext();
+            _repository = new MovieRepository(_context);
+            _controller = new MoviesRepController(_repository);
+        }
+
+        [Fact]
+        public void GetAllMovies_ReturnSuccessIfCorrectCount()
+        {
+            var result = _controller.GetMovies();
+
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var list=result.Result as OkObjectResult;
+
+            Assert.IsType<List<Movie>>(list.Value);
+
+            var movies = list.Value as List<Movie>;
+
+
+            Assert.Equal(_context.Movies.Count(), movies.Count);
+        }
+
+        [Fact]
+        public void GetAllMovies_ReturnSuccessIfWrongCount()
+        {
+            var result = _controller.GetMovies();
+
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var list = result.Result as OkObjectResult;
+
+            Assert.IsType<List<Movie>>(list.Value);
+
+            var movies = list.Value as List<Movie>;
+
+            Assert.NotEqual(2, movies.Count);
+        }
+
+        [Theory]
+        [InlineData(3, 666, "Full metal jacket")]
+        [InlineData(5, 777, "GI Joe")]
+        public void GetMovieById_ReturnsOkObjectResult(int id1, int id2, string title)
+        {
+            var okResult = _controller.GetMovie(id1);
+            var notFoundResult= _controller.GetMovie(id2);
+
+            Assert.IsType<OkObjectResult>(okResult.Result);
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+
+            var item= okResult.Result as OkObjectResult;
+
+            Assert.IsType<Movie>(item.Value);
+
+            var movie= item.Value as Movie;
+            Assert.Equal(title, movie.Title);
+            Assert.Equal(id1, movie.Id);
+        }
+
+
+        [Fact]
+        public void Add_InvalidObjectPassed_ReturnsBadRequest()
+        {
+            var newMovie = new Movie()
+            {
+                Genre = "Crime-Drama",
+                ReleaseYear = "1972"
+            };
+
+            _controller.ModelState.AddModelError("Title", "Required");
+
+            var badResponse = _controller.PostMovie(newMovie);
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+
+        [Fact]
+        public void Add_ValidObjectPassed_ReturnsCreatedResponse_And_Remove_GetExistingMovieById_ReturnsOkObjectResult()
+        {
+            var newMovie = new Movie()
+            {
+                Id = 333,
+                Title = "The Godfather",
+                Genre = "Crime-Drama",
+                ReleaseYear = "1972"
+            };
+
+            var createdResponse = _controller.PostMovie(newMovie);
+            Assert.IsType<CreatedAtActionResult>(createdResponse);
+
+            int id= ((createdResponse as CreatedAtActionResult).Value as Movie).Id;
+
+            var okResult = _controller.DeleteMovie(id);
+            Assert.IsType<OkObjectResult>(okResult);
+        }
+
+
+        [Theory]
+        [InlineData(99999)]
+        public void Remove_GetExistingMovieById_ReturnsNotFoundResult(int id)
+        {
+            var okResult = _controller.DeleteMovie(id);
+            Assert.IsType<NotFoundObjectResult>(okResult);
+        }
+
+    }
+}
